ae05d9be6bf8238a8dacb1fa96bed952b18d0d2f
Baliw02 <PlayWithBalazs2002@gmail.com>
2025-01-16 10:54:50 +0100

    elso

--- WindowsApplication2/Form1.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Form1
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MessageBox.Show("Naaaa?", "box", MessageBoxButtons.OK)
-    End Sub
-End Class
